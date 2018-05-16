890c9519ba1aac0e32fa52e0543fe9712d4124c8
bloya <l.loya.b@gmail.com>
2018-05-16 14:46:46 -0400

    membership update

--- contracts/Membership.sol
+++ contracts/Membership.sol
@@ -9,12 +9,7 @@ contract Membership {
     
     //Memebership fees
     uint public memberFee;
-    uint public investorFee;
-    uint public partnerFee;
-    
-    //Total membership tokens
-    uint public total_supply;
-    
+
     /*Structs*/
     /**
     *@dev Keeps member information 
@@ -32,7 +27,7 @@ contract Membership {
 
     
     /*Events*/
-    event Transfer(address _from, address _to, uint _tokenId);
+    event TransferFrom(address _from, address _to);
 
     /*Modifiers*/
     modifier onlyOwner() {
@@ -44,7 +39,7 @@ contract Membership {
     /**
     *@dev Constructor - Sets owner
     */
-    function Membership() public {
+    constructor () public {
         owner = msg.sender;
     }
 
@@ -54,51 +49,50 @@ contract Membership {
     *@param _investorFee fee amount for investor member
     *@param _partnerFee fee amount for member
     */
-    function setFee(uint _memberFee, uint _investorFee, uint _partnerFee) public onlyOwner() {
+    function setFee(uint _memberFee) public onlyOwner() {
         //define fee structure for the three membership types
         memberFee = _memberFee;
-        investorFee = _investorFee;
-        partnerFee = _partnerFee;
     }
     
     /**
     *@notice Allows a user to become DDA members if they pay the fee. However, they still have to complete
     complete KYC/AML verification off line
-    *@dev this creates the token 
+    *@dev this creates and transfers the token 
     *@return returns the newly created token
     */
     function requestMembership() public payable returns (uint) {
         Member storage sender = members[msg.sender];
-        require(msg.value >= memberFee && sender.membership == false);
-        //Update total supply of membership Tokens
-        total_supply = total_supply.add(1);
-        uint membershipToken = uint(keccak256(now, msg.sender, total_supply));
+        require(msg.value >= memberFee && sender.membershipType > 0);
+        uint membershipToken = membersAccts.length.add(1);
         sender.memberId = membershipToken;
         sender.membershipType = 1;
         sender.membership = true;
         membersAccts.push(msg.sender)-1;
         return membershipToken;
-        transferFrom(owner, msg.sender,membershipToken);
     }
     
-  /**
-  * @dev This overload transferFrom function is required on ERC721.org
-  * @param _from is the address that the token will be sent from
-  * @param _to is the address we are sending the token to
-  * @param _tokenId the uint256 numeric identifier of a token or the memberId
-  */
-  function transferFrom(address _from, address _to, uint256 _tokenId) public onlyOwner{
-    require (_to != address(0));
-    ownerMembershipCount[_from] = ownerMembershipCount[_from].sub(1);
-    ownerMembershipCount[_to] = ownerMembershipCount[_to].add(1);
-    //how do I send the actual tokenid/memberhipToken
-    emit Transfer(_from, _to, _tokenId);
-  }
+    /**
+    *@dev This overload transferFrom function is required on ERC721.org
+    *@param _from is the address that the token will be sent from
+    *@param _to is the address we are sending the token to
+    *@param _tokenId the uint256 numeric identifier of a token or the memberId
+    */
+    function transferFrom(address _from, address _to) public onlyOwner {
+        require (_to != address(0));
+        Member storage sender = members[_from];
+        Member storage receiver = members[_to];
+        receiver.memberId = sender.memberId;
+        receiver.membershipType = sender.membershipType;
+        receiver.membership = sender.membership;
+        sender.memberId = 0;
+        sender.membershipType = 0;
+        emit TransferFrom(_from, _to);
+    }
 
     /**
     *@dev Use this function to set membershipType for the member
-    @param _member address of member that we need to update membershipType
-    @param _membershipType type of membership to assign to member
+    *@param _member address of member that we need to update membershipType
+    *@param _membershipType type of membership to assign to member
     **/
     function setMembershipType(address _member,  uint _membershipType) public onlyOwner{
         Member storage member = members[_member];
@@ -113,27 +107,33 @@ contract Membership {
     }
     
     /**
-    @dev Get member information. could be the ownerOf funciton
-    @param _member address to pull the memberId, membershipType and membership
+    *@dev Get member information. could be the ownerOf funciton
+    *@param _member address to pull the memberId, membershipType and membership
     **/
     function getMember(address _member) view public returns(uint, uint, bool) {
         return(members[_member].memberId, members[_member].membershipType, members[_member].membership);
     }
 
     /**
-    @dev gets length of array containing all member accounts or total suply
+    @dev gets length of array containing all member accounts or total supply
     **/
     function countMembers() view public returns(uint) {
         return membersAccts.length;
     }
 
-    function getBalance(address _member) view public returns(uint) {
-        return ownerMembershipCount[_member];
+    /**
+    *@dev gets membership count per owner but it should always be 1
+    **/
+    function getMemberType(address _member) public constant returns(uint){
+        return members[_member].membershipType;
+    }
+    
+    /**
+    *@dev Allows the owner to set a new owner address
+    *@param _new_owner the new owner address
+    */
+    function setOwner(address _new_owner) public onlyOwner() { 
+        owner = _new_owner; 
     }
 
-        function getMemberType(address _member) public constant returns(uint){
-        return members[_member];
-    }
-    
 }
-    
--- test/testMembership.js
+++ test/testMembership.js
@@ -4,41 +4,44 @@ contract('Contracts', function(accounts) {
   let membership;
 
     it('Setup contract for testing', async function () {
-        membership = await Membership.new();
-        await membership.setFee(10, 15, 20);
-//        await membership.requestMembership() public payable returns (uint); 
-  //      await transferFrom(address _from, address _to, uint256 _tokenId);
-    //    await setMembershipType(address _member,  uint _membershipType);
-      //  await getMembers() view public returns;
-        //await getMember(address _member) view public returns(uint, uint, bool) ;
-        //await countMembers() view public returns(uint);
+        membership = await Membership.new({from: accounts[3]});
     });
 
     it("should send coin correctly", async function () {
     // Get initial balances of first and second account.
     var account_one = accounts[0];
     var account_two = accounts[1];
+    console.log("set up accts");
 
     var amount = 1;
+    console.log("set up amount");
 
-    let meta = await Membership.deployed();
-    let balance = await meta.requestMembership.call();
-    let balance1 = await meta.getBalance.call(account_one);
-    let balance2 = await meta.getBalance.call(account_two);
+
+    let fee =  await membership.setFee(10, {from: accounts[3]});
+    console.log("contract deployed");
+    let balance = await membership.requestMembership({from: accounts[0], value:web3.toWei(20, "ether")});
+        console.log("requestMembership");
+    let balance1 = await membership.getBalance(account_one);
+    let balance2 = await membership.getBalance(account_two);
+        console.log("get balances");
 
     let account_one_starting_balance = balance1.toNumber();
     let account_two_starting_balance = balance2.toNumber();
+            console.log("set balances");
 
-    await meta.transferFrom(account_one,account_two, amount);
+    await membership.transferFrom(account_one,account_two, amount);
+            console.log("tranferFrom");
 
-    let balance3 = await meta.getBalance.call(account_one);
-    let balance4 = await meta.getBalance.call(account_two);
-
+    let balance3 = await membership.getBalance(account_one);
+    let balance4 = await membership.getBalance(account_two);
+        console.log("get balances after tranferFrom");
     let account_one_ending_balance = balance3.toNumber();
     let account_two_ending_balance = balance4.toNumber();
-
+        console.log("set balances after from");
     assert.equal(account_one_ending_balance, account_one_starting_balance - 1, "Amount wasn't correctly taken from the sender");
+            console.log("assert1");
     assert.equal(account_two_ending_balance, account_two_starting_balance + 1, "Amount wasn't correctly sent to the receiver");
+            console.log("assert2");
   });
 
 
