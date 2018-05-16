de08e8fb8547d4bef35cb75d8966b73eb68bbf46
bloya <l.loya.b@gmail.com>
2018-05-16 14:57:35 -0400

    update to membership

--- test/testMembership.js
+++ test/testMembership.js
@@ -11,40 +11,32 @@ contract('Contracts', function(accounts) {
     // Get initial balances of first and second account.
     var account_one = accounts[0];
     var account_two = accounts[1];
+    var account_three = accounts[3];
     console.log("set up accts");
-
-    var amount = 1;
-    console.log("set up amount");
-
 
     let fee =  await membership.setFee(10, {from: accounts[3]});
     console.log("contract deployed");
-    let balance = await membership.requestMembership({from: accounts[0], value:web3.toWei(20, "ether")});
+    let member = await membership.requestMembership({from: accounts[0], value:web3.toWei(20, "ether")});
         console.log("requestMembership");
-    let balance1 = await membership.getBalance(account_one);
-    let balance2 = await membership.getBalance(account_two);
+
+
+    let member1 = await membership.getMemberType(account_one);
+    let member2 = await membership.getMemberType(account_two);
         console.log("get balances");
 
-    let account_one_starting_balance = balance1.toNumber();
-    let account_two_starting_balance = balance2.toNumber();
+    let account_one_starting_member = member1.toNumber();
+    let account_two_starting_member = member2.toNumber();
             console.log("set balances");
 
-    await membership.transferFrom(account_one,account_two, amount);
-            console.log("tranferFrom");
 
-    let balance3 = await membership.getBalance(account_one);
-    let balance4 = await membership.getBalance(account_two);
-        console.log("get balances after tranferFrom");
-    let account_one_ending_balance = balance3.toNumber();
-    let account_two_ending_balance = balance4.toNumber();
         console.log("set balances after from");
-    assert.equal(account_one_ending_balance, account_one_starting_balance - 1, "Amount wasn't correctly taken from the sender");
+    assert.equal(account_one_starting_member, 0, "From");
             console.log("assert1");
-    assert.equal(account_two_ending_balance, account_two_starting_balance + 1, "Amount wasn't correctly sent to the receiver");
+    assert.equal(account_two_starting_member, 1, "to");
             console.log("assert2");
-  });
+    });
 
 
 
         	
-    })
+})
